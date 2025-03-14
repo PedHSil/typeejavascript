639b3a735a82a961b4c0466a9ce9b44d8a1b864a
PedroH <pedroh422silva@gmail.com>
2025-03-14 10:02:23 -0300

    sav

---
+++ fase1/aula1.js
@@ -0,0 +1,9 @@
+let num1 = 1;
+let num2 = 2;
+
+console.log(num1.toString() + num2);
+console.log(typeof num1);
+console.log(num1.toString(2));
+
+let temp = num1 * 'ola';
+console.log(Number.isNaN(temp));
